e35ba5d245fbe54bcc580484459b5de0899fe580
Abdussalam Abdurakhimov <abduss@Abdussalams-MacBook-Air.local>
2025-06-21 11:44:13 +0500

    Upate logo

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -22,8 +22,8 @@ export default function LoginPage() {
       <Card className="mx-4 w-full max-w-md">
       <CardHeader className="space-y-1 flex flex-col items-center">
         {/* mb-2 вместо mb-4, и w-3/4 для ширины лого */}
-        <div className="mb-2 w-full h-64 relative">
-          <div className="flex h-full w-full items-center justify-center">
+        <div className="mb-1 w-full h-64 relative">
+          <div className="flex h-64 w-2/3 items-center justify-center">
             <object
               data="/dap.svg"
               type="image/svg+xml"
